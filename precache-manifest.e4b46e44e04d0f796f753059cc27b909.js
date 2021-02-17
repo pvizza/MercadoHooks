e8038e24efb6a1c79bab31e42ab1faae05948803
Pablo Baiz <pablobaiz@Pablos-MacBook-Pro.local>
2021-02-17 20:10:28 -0300

    Updates

---
+++ precache-manifest.e4b46e44e04d0f796f753059cc27b909.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "132ff6d2fa52059a6f8b1410c55456ae",
+    "url": "/MercadoHooks/index.html"
+  },
+  {
+    "revision": "1815faf7de607b41870f",
+    "url": "/MercadoHooks/static/css/main.686c9af3.chunk.css"
+  },
+  {
+    "revision": "9da44512a38af86ca11d",
+    "url": "/MercadoHooks/static/js/2.c0ca4ff4.chunk.js"
+  },
+  {
+    "revision": "8a3147a3ff4de6165e18776df6e4252f",
+    "url": "/MercadoHooks/static/js/2.c0ca4ff4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1815faf7de607b41870f",
+    "url": "/MercadoHooks/static/js/main.a84a18f0.chunk.js"
+  },
+  {
+    "revision": "ae8686fb9cfff38abc43",
+    "url": "/MercadoHooks/static/js/runtime-main.9f22c9c9.js"
+  }
+]);
